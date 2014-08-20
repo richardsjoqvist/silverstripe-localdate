b1153afa3accb99a30e106947a1b852a0dcb4ad5
Richard Sjoqvist <richard@spiro.se>
2014-08-20 13:19:23 +0200

    Added old-fashioned injection code to _config

--- _config.php
+++ _config.php
@@ -1,1 +1,5 @@
 <?php
+// Use old-fashioned way of injecting the custom class
+// because using the YAML-method does not always work
+Object::useCustomClass('Date', 'LocalDate');
+Object::useCustomClass('SS_Datetime', 'LocalDatetime');
