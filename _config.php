7287f70625de19432a9c0d3294dfe0965fbdde94
Florian Thoma <florian.thoma@innoweb.com.au>
2020-05-15 08:17:44 +1000

    add PHP 7.2 compatibility

--- _config.php
+++ _config.php
@@ -1,6 +1,9 @@
 <?php
+
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
+
 // Use old-fashioned way of injecting the custom class
 // because using the YAML-method does not always work
-Object::useCustomClass('Date', 'LocalDate');
-Object::useCustomClass('SS_Datetime', 'LocalDatetime');
-Object::useCustomClass('Time', 'LocalTime');
+SS_Object::useCustomClass('Date', 'LocalDate');
+SS_Object::useCustomClass('SS_Datetime', 'LocalDatetime');
+SS_Object::useCustomClass('Time', 'LocalTime');
